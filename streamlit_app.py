6fc63e0c837700b680f6ba7e7fbd53f7ff16a12e
bonniezl <109834849+bonniezl@users.noreply.github.com>
2022-08-04 13:52:21 -0700

    update the code for fruityvice_response
    removed the lien of raw JSON, and separate the base URL from the fruit name ( which will make it easier to use a variable there).

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# streamlit.text(fruityvice_response.json())
 
 
 # Normalize the fruitvice api response data from json version 
